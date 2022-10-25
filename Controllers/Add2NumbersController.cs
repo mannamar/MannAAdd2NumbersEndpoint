c68f39b613ace3c05009b045b48cea94c37ca029
Amardeep Mann <amar2slick@gmail.com>
2022-10-25 15:36:51 -0700

    Peer reviewed

--- Controllers/Add2NumbersController.cs
+++ Controllers/Add2NumbersController.cs
@@ -2,7 +2,7 @@
 // 10-25-22
 // Add2Numbers Endpoint
 // Added the ability to take in an input via web URL for our Add2Numbers function
-// Peer reviewed by
+// Peer reviewed by Reed Goodwin: Very well written program, the first one ive seen with any sort of validation. The way the validation was done was good, I like that the default math route is still there so you can test. ANd the UI is friendly! 
 
 using Microsoft.AspNetCore.Mvc;
 
